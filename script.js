5e6311cf37acfb896026f59c8274976a63b5b4f5
nguyenthaingocduy <87253154+nguyenthaingocduy@users.noreply.github.com>
2024-06-27 16:20:43 +0700

    update

--- script.js
+++ script.js
@@ -2,4 +2,14 @@ const menubarBtn = document.querySelector('.header-top i')
     menubarBtn.addEventListener("click", function(){
     document.querySelector('.header-top ul').classList.toggle('active')
 
+
 })
+
+function toggleContactOptions() {
+    var options = document.getElementById('contact-options');
+    if (options.classList.contains('show')) {
+        options.classList.remove('show');
+    } else {
+        options.classList.add('show');
+    }
+}
